82fcd4100d333e3b75534e9fd64432fd7491db37
Manasi <badhemanasi2@gmail.com>
2025-07-21 22:55:29 +0530

    design of heaader,footer and container without translator.

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular Project Starts';
+  images = [
+    'assets/banner.png',
+    'assets/banner3.jpg',
+    'assets/banner4.png'
+  ];
+ 
 }
